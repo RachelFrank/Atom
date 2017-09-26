d90264cf624939c44f25993624696ba6c74ef0af
Brent <puddletowndesign@gmail.com>
2017-09-26 13:00:41 -0500

    updated atom preferences

--- install.sh
+++ install.sh
@@ -3,6 +3,6 @@
 
 ln -s ~/Dev/Config/Atom ~/.atom
 
-git remote add upstream https://github.com/PuddletownDesign/Git
+git remote add upstream https://github.com/PuddletownDesign/Atom
 
 apm install --packages-file ~/Dev/Config/Atom/packages.list
